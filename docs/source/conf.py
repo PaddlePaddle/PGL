30dd42a0b3ee4338cf4a29ba3d8a617991172543
Yelrose <270018958@qq.com>
2019-10-10 16:39:13 +0800

    fixed docs v1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ copyright = '2019, PaddlePaddle'
 author = 'PaddlePaddle'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0.beta'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
