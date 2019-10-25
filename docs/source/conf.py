86295f697ce7b4c20bf063f96209b2bd5d9dfcb7
liweibin <liweibin02@baidu.com>
2019-10-25 16:14:58 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ copyright = '2019, PaddlePaddle'
 author = 'PaddlePaddle'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 
--- docs/source/examples/gat_examples_code.rst
+++ docs/source/examples/gat_examples_code.rst
@@ -1,8 +1,8 @@
 View the Code
 =============
 
-examples/gatne/main.py
+examples/gat/train.py
 
-.. literalinclude:: ../../../examples/gatne/main.py
+.. literalinclude:: ../../../examples/gat/train.py
     :language: python
     :linenos:
