b48a99fdb7eaf2a67966239d8c5b63d042be4730
DesmonDay <908660116@qq.com>
2020-06-15 15:36:13 +0800

    fix small bugs

--- examples/SAGPool/layers.py
+++ examples/SAGPool/layers.py
@@ -53,7 +53,7 @@ def topk_pool(gw, score, graph_id, ratio):
 
     index = L.arange(0, gw.num_nodes, dtype="int64")
     offset = L.gather(graph_lod, graph_id, overwrite=False)
-    index = (index - temp) + (graph_id * max_num_nodes)
+    index = (index - offset) + (graph_id * max_num_nodes)
     index.stop_gradient = True
     
     # padding
