1bdf429c245f925beebf6bd7da7a0fd59d76e540
Zhong Hui <zhonghui.net@gmail.com>
2020-07-02 01:04:15 +0800

    fix typo

--- pgl/tests/test_graph_saint_sample.py
+++ pgl/tests/test_graph_saint_sample.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ from pgl.sample import graph_saint_random_walk_sample
 
 
 class GraphSaintSampleTest(unittest.TestCase):
-    """ScatterAddTest"""
+    """GraphSaintSampleTest"""
 
     def test_randomwalk_sampler(self):
         """test_scatter_add"""
