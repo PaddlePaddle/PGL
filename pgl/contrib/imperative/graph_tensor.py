01c4b98488a24c4fa852c4b07c80cf093d7e2d2a
Zhong Hui <zhonghui.net@gmail.com>
2020-11-04 10:56:25 +0800

    refine graph tensor.

--- pgl/contrib/imperative/graph_tensor.py
+++ pgl/contrib/imperative/graph_tensor.py
@@ -14,6 +14,8 @@
 import paddle
 import paddle.fluid as fluid
 
+import numpy as np
+
 import pgl
 from pgl.utils import op
 from pgl.utils import paddle_helper
@@ -22,11 +24,48 @@ from pgl.math import segment_sum, segment_mean, segment_sum, segment_max
 import warnings
 
 
+class MessagePassing(object):
+    def __init__(self, graph_tensor, messages):
+        self._segment_ids = graph_tensor.dst
+        self._messages = messages
+
+    def reduce_sum(self, msg):
+        return segment_sum(msg, self._segment_ids)
+
+    def reduce_mean(self, msg):
+        return segment_mean(msg, self._segment_ids)
+
+    def reduce_max(self, msg):
+        return segment_max(msg, self._segment_ids)
+
+    def reduce_min(self, msg):
+        return segment_max(msg, self._segment_ids)
+
+    def sequence_expand(self, msg):
+        return paddle.gather(msg, dst, axis=0)
+
+    def reduce_sofmax(self, msg, beta=None):
+        if beta is not None:
+            msg = msg * beta
+        msg_max = self.reduce_max(msg)
+        msg_max = self.sequence_expand(msg_max)
+        msg = msg - msg_max
+        exp_msg = paddle.fluid.layers.exp(msg)
+
+        sum_exp_x = self.reduce_sum(exp_msg)
+        sum_exp_x = self.sequence_expand(sum_exp_x)
+
+        return exp_x / sum_exp_x
+
+    def __getitem__(self, key):
+        return self._messages[key]
+
+
 def send(src, dst, nfeat, efeat, message_func):
     """Send message from src to dst.
     """
-    src_feat = op.read_rows(nfeat, src)
-    dst_feat = op.read_rows(nfeat, dst)
+    src_feat = op.RowReader(nfeat, src)
+    dst_feat = op.RowReader(nfeat, dst)
     msg = message_func(src_feat, dst_feat, efeat)
     return msg
 
@@ -65,83 +104,79 @@ def recv(dst, uniq_dst, msg, reduce_function, num_nodes, num_edges):
     return output
 
 
-class GraphTensor(pgl.graph.Graph):
+class GraphTensor(pgl.graph_wrapper.BaseGraphWrapper):
     """Copy the graph object to anonymous shared memory.
     """
 
     def __init__(self, graph):
-        degree = graph.indegree()
-
-        #degree = graph.outdegree()
-        def share_feat(feat):
-            for key in feat:
-                feat[key] = paddle.to_tensor(feat[key])
-            return feat
-
-        def share_adj_index(index):
-            if index is not None:
-                index._degree = paddle.to_tensor(index._degree)
-                index._sorted_u = paddle.to_tensor(index._sorted_u)
-                index._sorted_v = paddle.to_tensor(index._sorted_v)
-                index._sorted_eid = paddle.to_tensor(index._sorted_eid)
-                index._indptr = paddle.to_tensor(index._indptr)
-            return index
-
-        self._num_nodes = graph._num_nodes
-        # src, dst, eid = graph.sorted_edges(sort_by="dst")
-        # self._edges_src = paddle.to_tensor(src)
-        # self._edges_dst = paddle.to_tensor(dst)
-        self._uniq_dst = paddle.to_tensor(graph.nodes[graph.indegree() > 0])
-        self._edges_src = paddle.to_tensor(graph._edges[:, 0])
-        self._edges_dst = paddle.to_tensor(graph._edges[:, 1])
-        # self._edges = paddle.to_tensor(graph._edges)
-        self._adj_src_index = share_adj_index(graph._adj_src_index)
-        self._adj_dst_index = share_adj_index(graph._adj_dst_index)
-        self._node_feat = share_feat(graph._node_feat)
-        self._edge_feat = share_feat(graph._edge_feat)
-        self._num_edges = len(graph._edges)
-
-        # self.src, self.dst = self.edges
-        # self.dst, self.src = self._adj_src_index._sorted_u, self._adj_src_index._sorted_v
-        # self.dst, self.src = self._adj_dst_index._sorted_u, self._adj_dst_index._sorted_v
-        self.dst, self.src = self._adj_dst_index._sorted_u, self._adj_dst_index._sorted_v
-
-    def send(self, message_func, nfeat_list=None, efeat_list=None):
-        if efeat_list is None:
-            efeat_list = {}
-        if nfeat_list is None:
-            nfeat_list = {}
-
-        nfeat = {}
-        for feat in nfeat_list:
-            if isinstance(feat, str):
-                nfeat[feat] = self.node_feat[feat]
-            else:
-                name, tensor = feat
-                nfeat[name] = tensor
-
-        efeat = {}
-        for feat in efeat_list:
-            if isinstance(feat, str):
-                efeat[feat] = self.edge_feat[feat]
-            else:
-                name, tensor = feat
-                efeat[name] = tensor
-
-        msg = send(self.src, self.dst, nfeat, efeat, message_func)
-        return msg
+        super(GraphTensor, self).__init__()
+        self.__create_graph_attr(graph)
 
     def recv(self, msg, reduce_function):
         output = recv(
-            # dst=self._edges_dst,
-            dst=self.dst,
-            uniq_dst=self._uniq_dst,
+            dst=self._edges_dst,
+            uniq_dst=self._edge_uniq_dst,
             msg=msg,
             reduce_function=reduce_function,
             num_edges=self._num_edges,
             num_nodes=self._num_nodes)
         return output
 
-    @property
-    def edges(self):
-        return self._edges_src, self._edges_dst
+    def __create_graph_attr(self, graph):
+        """Create graph attributes for paddlepaddle.
+        """
+        src, dst, eid = graph.sorted_edges(sort_by="dst")
+        indegree = graph.indegree()
+        nodes = graph.nodes
+        uniq_dst = nodes[indegree > 0]
+        uniq_dst_count = indegree[indegree > 0]
+        uniq_dst_count = np.cumsum(uniq_dst_count, dtype='int32')
+        uniq_dst_count = np.insert(uniq_dst_count, 0, 0)
+        graph_lod = graph.graph_lod
+        num_graph = graph.num_graph
+
+        num_edges = len(src)
+        if num_edges == 0:
+            # Fake Graph
+            src = np.array([0], dtype="int64")
+            dst = np.array([0], dtype="int64")
+            eid = np.array([0], dtype="int64")
+            uniq_dst_count = np.array([0, 1], dtype="int32")
+            uniq_dst = np.array([0], dtype="int64")
+
+        edge_feat = {}
+
+        for key, value in graph.edge_feat.items():
+            edge_feat[key] = value[eid]
+        node_feat = graph.node_feat
+
+        self.__create_graph_node_feat(node_feat)
+        self.__create_graph_edge_feat(edge_feat)
+
+        self._num_edges = paddle.to_tensor(
+            np.array(
+                [num_edges], dtype="int64"))
+        self._num_graph = paddle.to_tensor(
+            np.array(
+                [num_graph], dtype="int64"))
+        self._edges_src = paddle.to_tensor(src)
+        self._edges_dst = paddle.to_tensor(dst)
+        self._num_nodes = paddle.to_tensor(np.array([graph.num_nodes]))
+        self._edge_uniq_dst = paddle.to_tensor(uniq_dst)
+        self._edge_uniq_dst_count = paddle.to_tensor(uniq_dst_count)
+        self._graph_lod = paddle.to_tensor(graph_lod)
+        self._indegree = paddle.to_tensor(indegree)
+
+    def __create_graph_node_feat(self, node_feat):
+        """Convert node features into paddlepaddle tensor.
+        """
+        for node_feat_name, node_feat_value in node_feat.items():
+            self.node_feat_tensor_dict[node_feat_name] = paddle.to_tensor(
+                node_feat_value)
+
+    def __create_graph_edge_feat(self, edge_feat):
+        """Convert edge features into paddlepaddle tensor.
+        """
+        for edge_feat_name, edge_feat_value in edge_feat.items():
+            self.edge_feat_tensor_dict[edge_feat_name] = paddle.to_tensor(
+                edge_feat_value)
