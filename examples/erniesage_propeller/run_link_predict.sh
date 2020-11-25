052c269e0d83ee8804b499e8161ef87939f02814
Weiyus Su <weiyue.su@gmail.com>
2020-11-26 00:11:54 +0800

    rewrite encoder

--- examples/erniesage_propeller/run_link_predict.sh
+++ examples/erniesage_propeller/run_link_predict.sh
@@ -2,9 +2,8 @@
 
 config=${1:-"./config/erniesage_link_predict.yaml"}
 
-source ~/.zshrc
-#python ./preprocessing/dump_graph.py --conf $config
+python ./preprocessing/dump_graph.py --conf $config
 
 unset http_proxy https_proxy
 python -m paddle.distributed.launch link_predict.py --conf $config
-#python -m paddle.distributed.launch link_predict.py --conf $config --do_predict
+python -m paddle.distributed.launch link_predict.py --conf $config --do_predict
