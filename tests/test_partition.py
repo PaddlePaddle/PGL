de6179561c337532de2f9cc7fdd38f582a682732
yelrose <270018958@qq.com>
2021-08-13 18:00:37 +0800

    change

--- tests/test_partition.py
+++ tests/test_partition.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 import unittest
@@ -12,9 +26,10 @@ from testsuite import create_random_graph
 class GraphPartitionTest(unittest.TestCase):
     """ Test METIS Partition
     """
+
     def test_partition_graph(self):
         glist = []
-        npart = 20 
+        npart = 20
         for i in range(npart):
             num_nodes = 10
             edges = np.random.randint(
@@ -23,9 +38,7 @@ class GraphPartitionTest(unittest.TestCase):
                 size=[np.random.randint(
                     low=1, high=10), 2])
 
-            g = pgl.Graph(
-                edges=edges,
-                num_nodes=num_nodes)
+            g = pgl.Graph(edges=edges, num_nodes=num_nodes)
             glist.append(g)
 
         # Merge Graph
@@ -34,10 +47,10 @@ class GraphPartitionTest(unittest.TestCase):
         cluster_id = metis_partition(multi_graph, npart=npart)
         ori_graph_id = multi_graph.graph_node_id
         for i in range(npart):
-            cluster = cluster_id[ori_graph_id  == i]
+            cluster = cluster_id[ori_graph_id == i]
             # The partition should be in same within a cluster
             self.assertTrue(np.any(cluster != cluster[0]))
-        
+
 
 if __name__ == "__main__":
     unittest.main()
