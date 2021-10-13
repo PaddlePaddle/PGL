54b10db72560eb2c252f7c05ee15d0f3dff2984e
Huijuan Wang <wanghuijuan03@baidu.com>
2021-10-13 16:27:46 +0800

    [Scripts] add scripts

---
+++ apps/graph4kg/examples/fb15k.sh
@@ -0,0 +1,17 @@
+# TransE single GPU
+python -u main.py --score TransE --dataset FB15k --data_path ~/data --save_path output/transe_fb_sgpu \
+--batch_size 1000 --test_batch_size 16 --log_interval 1000 --eval_interval 24000 --reg_coef 1e-9 --reg_norm 3 \
+--num_negs 1 --neg_mode 'full' --hidden_dim 400 --gamma 19.9 --lr 0.25 -adv \
+--num_workers 8 --num_epoch 100 --valid --test --print_on_screen
+
+# TransE test
+python -u link_prediction.py output/transe_fb_sgpu/TransE_FB15k_d_400_g_19.9
+
+# RotatE single GPU
+python -u main.py --score RotatE --dataset FB15k --data_path ~/data --save_path output/rotate_fb_sgpu \
+--batch_size 2048 --test_batch_size 16 --log_interval 1000 --eval_interval 20000 --reg_coef 1e-7 --reg_norm 3 \
+--num_negs 1 --neg_mode 'full' --hidden_dim 200 --gamma 12.0 --lr 0.009 -adv \
+--num_workers 8 --num_epoch 100 --valid --test --print_on_screen --ent_times 2 
+
+# RotatE test
+python -u link_prediction.py output/rotate_fb_sgpu/RotatE_FB15k_d_200_g_12.0
---
+++ apps/graph4kg/examples/fb15k_profile.sh
@@ -0,0 +1,28 @@
+"""
+import os
+import paddle
+
+    if step == 100:
+		paddle.core.nvprof_start()
+		paddle.core.nvprof_enable_record_event()
+		paddle.core.nvprof_nvtx_push(str(step))
+	if step == 110:
+		paddle.core.nvprof_nvtx_pop()
+		paddle.core.nvprof_stop()
+		sys.exit()
+	if step > 100 and step < 110:
+		paddle.core.nvprof_nvtx_pop()
+		paddle.core.nvprof_nvtx_push(str(step))
+"""
+
+# TransE single GPU
+nsys profile -t cuda,nvtx -o transe_fb15k --capture-range=cudaProfilerApi --stop-on-range-end=true python -u main.py \
+--score TransE --dataset FB15k --data_path ~/data --save_path output/transe_fb_sgpu \
+--batch_size 1000 --reg_coef 1e-9 --reg_norm 3 --num_negs 1 --neg_mode 'full' --hidden_dim 400 --gamma 19.9 \
+--lr 0.25 -adv --num_workers 8 --num_epoch 2
+
+# RotatE single GPU
+nsys profile -t cuda,nvtx -o rotate_fb15k --capture-range=cudaProfilerApi --stop-on-range-end=true python -u main.py \
+--score RotatE --dataset FB15k --data_path ~/data --save_path output/rotate_fb_sgpu \
+--batch_size 2048 --reg_coef 1e-7 --reg_norm 3 --num_negs 1 --neg_mode 'full' --hidden_dim 200 --gamma 12.0 \
+--lr 0.009 -adv --num_workers 8 --num_epoch 2 --ent_times 2 
