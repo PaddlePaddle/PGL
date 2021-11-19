43cf81e52ae050f460381dd33183d8bfbe440016
Zhong Hui <zhonghui.net@gmail.com>
2021-11-19 21:14:51 +0800

    fix typo

--- pgl/math.py
+++ pgl/math.py
@@ -79,7 +79,7 @@ def segment_softmax(data, segment_ids):
             data = [[1, 2, 3], 
                     [3, 2, 1], 
                     [4, 5, 6]]
-            data = paddle.to_tensor(, dtype='float32')
+            data = paddle.to_tensor(data, dtype='float32')
             segment_ids = paddle.to_tensor([0, 0, 1], dtype='int32')
             out = pgl.math.segment_softmax(data, segment_ids)
 
