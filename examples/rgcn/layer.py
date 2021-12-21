79a63580fb12a816fb1ec36b40ce1e92e4dc3bc7
sunzhuo <46882246+zsun1029@users.noreply.github.com>
2021-12-21 20:56:34 +0800

    Update layer.py

--- examples/rgcn/layer.py
+++ examples/rgcn/layer.py
@@ -1,16 +1,17 @@
-#!usr/bin/env python
-# -*- coding:utf-8 -*-
-"""
-/**************************************************************
- * Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
- * @author: sunzhuo02 
- * @email: sunzhuo02@baidu.com
- * @dept: KG
- * @file: layer.py
- * @time: 2021-12-06
- * @description: 
-**************************************************************/
-"""
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import math
 import numpy as np
 import paddle.fluid as fluid
