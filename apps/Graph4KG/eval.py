1b38528a0105deab0b01df92cb7e7190c35e8cfb
wawltor <fangzeyang0904@hotmail.com>
2022-03-16 09:30:00 +0800

    fix some problem for the kg

--- apps/Graph4KG/eval.py
+++ apps/Graph4KG/eval.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ def main():
     if args.init_from_ckpt:
         state_dict = paddle.load(
             os.path.join(args.init_from_ckpt, 'params.pdparams'))
-    model.set_dict(state_dict)
+        model.set_dict(state_dict)
     evaluate(
         model,
         test_loader,
