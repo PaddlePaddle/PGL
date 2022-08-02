cb9fb4bfa6d05f9dcf1221dff73c11af80e12fa7
liweibin02 <liweibin02@baidu.com>
2022-08-02 14:36:14 +0800

    fix the conflict of numpy version between paddle and pgl

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ copyright = '2021, PaddlePaddle'
 author = 'PaddlePaddle'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.3post0'
+release = '2.2.4'
 
 # -- General configuration ---------------------------------------------------
 
