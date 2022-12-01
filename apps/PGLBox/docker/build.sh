0c04fb755a8db38402c633fd10e911a6e26529db
huangzhengjie <huangzhengjie@baidu.com>
2022-12-01 16:05:02 +0800

    add pglbox

---
+++ apps/PGLBox/docker/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+BASE_BUILD_IMAGE=registry.baidubce.com/paddlepaddle/paddle_manylinux_devel:cuda11.0-cudnn8
+TAG_NAME=pglbox
+
+docker build --network host -t ${TAG_NAME} -f ./Dockerfile . 
