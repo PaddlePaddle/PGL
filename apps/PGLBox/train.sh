61bc8ff0d20ab3373deb9e3089a08a67a9b03312
liweibin02 <liweibin02@baidu.com>
2022-12-06 16:45:41 +0800

    add sharding tool

--- apps/PGLBox/train.sh
+++ apps/PGLBox/train.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-pushd src
+SOURCE_HOME=$(readlink -f $(dirname ${BASH_SOURCE[0]}) )/
+
+LOG_DIR=${SOURCE_HOME}/train_logs
+[ ! -d ${LOG_DIR} ] && mkdir -p ${LOG_DIR}
+
+config_file=$1
+cp ${config_file} ${SOURCE_HOME}/src/config.yaml
+
+pushd ${SOURCE_HOME}/src
 source ./env.sh
-python cluster_train_and_infer.py
+python cluster_train_and_infer.py 2>&1 | tee ${LOG_DIR}/run.log
 popd 
